bd635e2fd045f4de92a73e421096a5a8db9346ec
nanakochi123456 <yy845jp@gmail.com>
2014-10-16 08:32:59 +0900

    5.0

---
+++ h3_12.bat
@@ -0,0 +1,4 @@
+y:
+cd "Program\Timidity++\Timidity++"
+start timidity -c ../patches/cfgs/h3_12.cfg -Ow --output-stereo -s 48000 --reverb=d,127 e:\bgm\h3_12\h3_12.mid
+timidity -c ../patches/cfgs/h3_12.cfg --output-stereo -s 48000 --reverb=d,127 e:\bgm\h3_12\h3_12.mid
